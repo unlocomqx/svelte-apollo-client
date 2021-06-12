ebfb2b0f2b7459297fe020c78056bbc036337bac
unlocomqx <amxmodx.unloco@gmail.com>
2021-06-12 13:49:06 +0100

    Add sveltekit example app

---
+++ app/src/routes/todos/[uid].json.ts
@@ -0,0 +1,16 @@
+import type { Locals } from "$lib/types";
+import type { RequestHandler } from "@sveltejs/kit";
+import { api } from "./_api";
+
+// PATCH /todos/:uid.json
+export const patch: RequestHandler<Locals, FormData> = async (request) => {
+	return api(request, `todos/${ request.locals.userid }/${ request.params.uid }`, {
+		text: request.body.get("text"),
+		done: request.body.has("done") ? !!request.body.get("done") : undefined
+	});
+};
+
+// DELETE /todos/:uid.json
+export const del: RequestHandler<Locals> = async (request) => {
+	return api(request, `todos/${ request.locals.userid }/${ request.params.uid }`);
+};
