b9d83763116e63f17ba57da46ee55adb00e7f3d4
unlocomqx <amxmodx.unloco@gmail.com>
2021-07-13 21:05:18 +0100

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { SvelteApolloClient } from "./client";
+export type { ReadableQuery, ReadableResult } from "./observable";
+export type { MutateOptions } from "./mutation";
+export type { Restoring } from "./restore";
