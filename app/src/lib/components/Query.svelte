6884300f306ac505ca4dd4dc03fcfe7bbd90d5ba
unlocomqx <amxmodx.unloco@gmail.com>
2021-07-17 12:25:13 +0100

    Create a separate client for querying

--- app/src/lib/components/Query.svelte
+++ app/src/lib/components/Query.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
-
-	import { client } from "$lib/client";
+	import { InMemoryCache } from "@apollo/client/cache";
 	import { gql } from "@apollo/client/core";
+	import { SvelteApolloClient } from "svelte-apollo-client";
+
+	export const client = SvelteApolloClient({
+		uri  : "https://48p1r2roz4.sse.codesandbox.io",
+		cache: new InMemoryCache()
+	});
 
 	let rates;
 
